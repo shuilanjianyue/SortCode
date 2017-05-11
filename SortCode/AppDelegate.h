cfe9c754bb10a2e733cc42414c906646c15d5cca
dazaoqiancheng <sun_cun_hua@163.com>
2017-05-11 08:54:53 +0800

    Initial Commit

---
+++ SortCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SortCode
+//
+//  Created by dazaoqiancheng on 2017/5/11.
+//  Copyright © 2017年 DZQC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SortCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SortCode
+//
+//  Created by dazaoqiancheng on 2017/5/11.
+//  Copyright © 2017年 DZQC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
